7048c6ad371f27a0348f43c9d6ee5f8a18cb0b95
GeorgeBinDragon <GeorgeBinDragon@gmail.com>
2019-08-15 16:20:28 +0800

    add some device properties

--- utils_android/src/main/java/com/georgebindragon/base/data/constants/Manufacturers.java
+++ utils_android/src/main/java/com/georgebindragon/base/data/constants/Manufacturers.java
@@ -20,6 +20,7 @@ public class Manufacturers
 {
 	//
 	public static final String Manufacturer = DeviceUtil.getManufacturer();
+	public static final String Model        = DeviceUtil.getModel();
 
 	//知名手机厂商
 	public static final String Manufacturer_Xiaomi  = "Xiaomi";//小米
@@ -38,6 +39,7 @@ public class Manufacturers
 	//其他厂商
 	//常见的分辨不出来的厂商
 	public static final String Manufacturer_alps = "alps";
+	public static final String Model_TELO        = "TELO";//意云的机型-->不能用Media播放提示音
 
 	//行业内厂商
 	//模块商
@@ -53,12 +55,16 @@ public class Manufacturers
 
 	public static boolean isMediaPlayerDisable()
 	{
+		if (isSomeManufacturer(Manufacturer_alps))
+		{
+			if (Model.contains(Model_TELO)) return true;
+		}
 		return isSomeManufacturer(Manufacturer_samsung);
 	}
 
 	public static boolean isSoundPoolDisable()
 	{
-		return isSomeManufacturer(Manufacturer_UNIPRO, Manufacturer_ALK, Manufacturer_HF, Manufacturer_Quiswise);
+		return isSomeManufacturer(Manufacturer_UNIPRO, Manufacturer_ELINK, Manufacturer_ALK, Manufacturer_HF, Manufacturer_Quiswise);
 	}
 
 	public static boolean isNormalCellPhone()
