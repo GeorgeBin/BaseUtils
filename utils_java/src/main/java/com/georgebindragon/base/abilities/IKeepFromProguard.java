891f4e91cf53531f222946f57e09f0e4716fe74f
GeorgeBinDragon <GeorgeBinDragon@gmail.com>
2019-10-10 15:16:55 +0800

    add proguard keep interface

---
+++ utils_java/src/main/java/com/georgebindragon/base/abilities/IKeepFromProguard.java
@@ -0,0 +1,18 @@
+package com.georgebindragon.base.abilities;
+
+/**
+ * 创建人：George
+ * 类名称：IKeepFromProguard
+ * 类概述：
+ * 详细描述：
+ *
+ *
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+
+
+public interface IKeepFromProguard extends IAbilities
+{
+}
