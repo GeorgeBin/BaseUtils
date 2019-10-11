b62c26e3c55ffac86d17e1536b46397b579cae69
GeorgeBinDragon <GeorgeBinDragon@gmail.com>
2019-10-11 19:55:58 +0800

    add a method to change EditView password show or hide

--- utils_android/src/main/java/com/georgebindragon/base/system/ui/ViewUtil.java
+++ utils_android/src/main/java/com/georgebindragon/base/system/ui/ViewUtil.java
@@ -1,9 +1,13 @@
 package com.georgebindragon.base.system.ui;
 
 import android.content.Context;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
 import com.georgebindragon.base.BaseUtils;
 
@@ -30,5 +34,30 @@ public class ViewUtil
 
 		LayoutInflater from = LayoutInflater.from(context);
 		return from.inflate(res, rootView);
+	}
+
+
+	//设置密码可见和不可见
+	public static void setPasswordVisibility(EditText editText, boolean setVisible)
+	{
+		if (null != editText)
+		{
+			int selectionEnd = editText.getSelectionEnd();
+			if (setVisible)
+			{
+				// 设置为可见
+				editText.setInputType(EditorInfo.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+				editText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+			} else
+			{
+				// 设置为不可见
+				editText.setInputType(EditorInfo.TYPE_TEXT_VARIATION_PASSWORD);
+				editText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+			}
+
+			int length = editText.getText().toString().length();
+
+			editText.setSelection((selectionEnd > length ? length : selectionEnd));//执行上面的代码后光标会处于输入框的最前方-->重置光标位置
+		}
 	}
 }
