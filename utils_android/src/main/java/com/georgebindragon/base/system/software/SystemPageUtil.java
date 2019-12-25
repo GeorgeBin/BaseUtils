0ad48371de114885cb1b2f7a62d65f56f2dd78f4
GeorgeBinDragon <GeorgeBinDragon@gmail.com>
2019-12-25 16:47:09 +0800

    fix jumpToSettingPage and add method jumpToSettingPage_App to jump to setting page of this packageName

--- utils_android/src/main/java/com/georgebindragon/base/system/software/SystemPageUtil.java
+++ utils_android/src/main/java/com/georgebindragon/base/system/software/SystemPageUtil.java
@@ -2,18 +2,17 @@ package com.georgebindragon.base.system.software;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.Settings;
 
+import com.georgebindragon.base.BaseUtils;
 import com.georgebindragon.base.function.log.LogProxy;
-import com.georgebindragon.base.utils.EmptyUtil;
+import com.georgebindragon.base.utils.StringUtil;
 
 /**
  * 创建人：George
- * 类名称：SystemPageUtil
- * 类概述：
- * 详细描述：
  *
- *
+ * 描述：
  *
  * 修改人：
  * 修改时间：
@@ -25,48 +24,48 @@ public class SystemPageUtil
 {
 	private static final String TAG = "SystemPageUtil-->";
 
+	public static boolean jumpToSettingPage_App(String packageName)
+	{
+		try
+		{
+			Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+			Uri    uri    = Uri.fromParts("package", packageName, null);
+			intent.setData(uri);
+
+			boolean find = ActivityUtil.hasActivityIntent(BaseUtils.getContext(), intent);
+			if (!find) intent = new Intent(Settings.ACTION_MANAGE_ALL_APPLICATIONS_SETTINGS);
+
+			return ActivityUtil.jumpActivity(BaseUtils.getContext(), intent);
+		} catch (Exception e)
+		{
+			LogProxy.e(TAG, "jumpToSettingPage_App", e);
+		}
+		return false;
+	}
+
 	/**
 	 * 打开 Gps 设置界面
 	 */
-	public static boolean jumpToGpsSettingPage(Context context)
+	public static boolean jumpToSettingPage_Location(Context context)
 	{
-		return jumpToXXXSettingPage(context, Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+		return jumpToSettingPage(context, Settings.ACTION_LOCATION_SOURCE_SETTINGS);
 	}
 
 	/**
 	 * 打开 Accessibility 设置界面
 	 */
-	public static boolean jumpToAccessibilitySettingPage(Context context)
+	public static boolean jumpToSettingPage_Accessibility(Context context)
 	{
-		return jumpToXXXSettingPage(context, Settings.ACTION_ACCESSIBILITY_SETTINGS);
+		return jumpToSettingPage(context, Settings.ACTION_ACCESSIBILITY_SETTINGS);
 	}
 
 	/**
 	 * 打开 系统 设置界面
 	 */
-	public static boolean jumpToXXXSettingPage(Context context, String action)
+	public static boolean jumpToSettingPage(Context context, String action)
 	{
-		Intent intent = new Intent(action);
-		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		boolean isSuccess = jumpPage(context, intent);
-		LogProxy.d(TAG, "jumpToXXXSettingPage", "Action=" + action + " 跳转成功与否=" + Boolean.toString(isSuccess));
-		return isSuccess;
-	}
-
-	public static boolean jumpPage(Context context, Intent intent)
-	{
-		if (EmptyUtil.notEmpty(context, intent))
-		{
-			try
-			{
-				context.startActivity(intent);
-				return true;
-			} catch (Exception e)
-			{
-				LogProxy.e(TAG, "jumpPage", e);
-				return false;
-			}
-		}
-		return false;
+		boolean success = ActivityUtil.jumpActivity(context, new Intent(action));
+		LogProxy.d(TAG, "jumpToSettingPage-->action=" + StringUtil.getPrintString(action), "跳转：" + StringUtil.getSuccessString(success));
+		return success;
 	}
 }
