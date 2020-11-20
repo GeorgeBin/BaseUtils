5e092e45936dfccf4774f0e21a5114090815081a
GeorgeBinDragon <GeorgeBinDragon@gmail.com>
2020-11-20 15:17:21 +0800

    add method to get application

--- utils_android/src/main/java/com/georgebindragon/base/BaseUtils.java
+++ utils_android/src/main/java/com/georgebindragon/base/BaseUtils.java
@@ -1,6 +1,7 @@
 package com.georgebindragon.base;
 
 import android.annotation.SuppressLint;
+import android.app.Application;
 import android.content.Context;
 
 import com.georgebindragon.base.android.log.LogFunctionAndroidImp;
@@ -21,14 +22,14 @@ import com.georgebindragon.base.thread.HandlerUtil;
  *
  * 定义:
  *
- *  Proxy: 代理, 能切换具体实现类 或者 能进行解耦的代理
+ * Proxy: 代理, 能切换具体实现类 或者 能进行解耦的代理
  *
- *  Function: 功能, 库本身封装的功能
- *  Executive: 管理者, 一般是调用系统的功能
- *  Monitor: 监听器, 用于Listener 和 CallBack的管理者
+ * Function: 功能, 库本身封装的功能
+ * Executive: 管理者, 一般是调用系统的功能
+ * Monitor: 监听器, 用于Listener 和 CallBack的管理者
  *
- *  Listener: 可注册多个
- *  CallBack: 只可以注册一个, 或回调一次
+ * Listener: 可注册多个
+ * CallBack: 只可以注册一个, 或回调一次
  *
  * 修改人：
  * 修改时间：
@@ -38,6 +39,8 @@ import com.georgebindragon.base.thread.HandlerUtil;
 @SuppressLint("StaticFieldLeak")
 public class BaseUtils
 {
+	private static final String TAG = "BaseUtils-->";
+
 	private static Context context;
 
 	public static void init(Context context2)
@@ -53,8 +56,26 @@ public class BaseUtils
 		LanguageUtil.init(context2);
 	}
 
-	public static Context getContext()
+	public static Context getContext() { return context; }
+
+	public static Application getApplication()
 	{
-		return context;
+		if (context instanceof Application) return (Application) context;
+		return getApplicationByReflect();
 	}
+
+	@SuppressLint("PrivateApi")
+	private static Application getApplicationByReflect()
+	{
+		try
+		{
+			Class<?> activityThread = Class.forName("android.app.ActivityThread");
+			Object   thread         = activityThread.getMethod("currentActivityThread").invoke(null);
+			Object   app            = activityThread.getMethod("getApplication").invoke(thread);
+
+			return (Application) app;
+		} catch (Exception e) { LogProxy.e(TAG, "getApplicationByReflect", e); }
+		return null;
+	}
+
 }
