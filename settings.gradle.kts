78f407690fc34a74b03527d9575cceb98eefcf23
GeorgeBinDragon <georgebindragon@gmail.com>
2024-06-11 16:49:17 +0800

    add maven proxy

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,12 +19,22 @@ dependencyResolutionManagement {
   repositories {
     google()
     mavenCentral()
-    maven("https://jitpack.io") //https://github.com/CymChad/BaseRecyclerViewAdapterHelper 需要
-    jcenter()// = http://jcenter.bintray.com
+    maven("https://jitpack.io")
+    maven("https://repo1.maven.org/maven2/")
+    // maven mirror
+    // 阿里云 https://developer.aliyun.com/mvn/guide
+    maven("https://maven.aliyun.com/repository/google")
+    maven("https://maven.aliyun.com/repository/public")
+    // 华为 https://mirrors.huaweicloud.com
+    maven("https://repo.huaweicloud.com/repository/maven/")
+    // 网易 http://mirrors.163.com/
+    // maven { url "https://mirrors.163.com/maven/repository/maven-public/" }
+    // 腾讯 https://mirrors.cloud.tencent.com/
+    // maven { url "http://mirrors.tencent.com/nexus/repository/maven-public/" }
   }
 }
 
-rootProject.name = "baseutils"
+rootProject.name = "BaseUtils"
 
 include(":utils_java")
 include(":utils_android")
